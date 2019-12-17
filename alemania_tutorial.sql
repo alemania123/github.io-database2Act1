2775fcd0236b9fb45783a8af4182ea910d9b22a0
alemania123 <58455076+alemania123@users.noreply.github.com>
2019-12-17 14:07:39 +0800

    Add files via upload

---
+++ alemania_tutorial.sql
@@ -0,0 +1,48 @@
+-- insert into public.review
+-- values
+-- ('S5323', 'Bill Gates', 'T110', 'MARQUEZ, GIANCARLO B.', 10.00, '1-Jan-2020','T10'),
+
+-- ('S1521', 'Susan Wojcicki', 'T102', 'BANDONG, KENTROMANS S.', 9.00, '5-Jan-2020','T12'),
+
+-- ('S1091', 'Jack PatrickDorsey','T109', 'LONGHAS, MICHAELBRYAN P.', 16.00, '15-Jan-2020','T06'),
+
+-- ('S1008', 'Yves Guillemot', 'T104', 'NICOLAS, JENO M.', 16.30, '6-Jan-2020', 'T06'),
+
+-- ('S1004', 'Bob Iger', 'T103', 'PALMA, JUSTIN LLOYDC.', 10.00, '15-Jan-2020', 'T99'),
+
+-- ('S1011', 'Mark Zuckerberg','', 'T100 BARCENA, CZEDRIX C.', 1.00, '15-Jan-2020','T06'),
+
+-- ('S2000', 'Jack Ma', 'T201', 'LALIS, JULLYLLAMARIE M.', 23.00, '15-Jan-2020' , 'T10'),
+
+-- ('S1023', 'Jeff Bezos', 'T107', 'PISAO, LAIMEEANTONETTE R.', 10.00, '15-Jan-2020', 'T52'),
+
+
+-- ('S2939', 'Sundar Pichai', 'T101', 'CABAGTI, KIMBERLY L.', 9.00,  '8-Jan-2020', 'T01'),
+
+-- ('S9829', 'Linus Torvalds', 'T108', 'GULANES, GENE JR. E.', 16.00, '15-Jan-2020', 'T12'),
+
+-- ('S1091', 'Jack PatrickDorsey','T109', 'SULLAN, JOSEPHBENEDICT D.', 16.30, '12-Jan-2020', 'T06'),
+
+-- ('S1232', 'Satya Nadella', 'T105', 'DELGADO, JHONKRISTOFFER U.', 10.00, '15-Jan-2020',  'T10'),
+
+
+-- ('S9829', 'Linus Torvalds', 'T108', 'VILLAMIL, RONELLCRIZ S.', 1.00,  '15-Jan-2020', 'T99'),
+
+-- ('S1008', 'Yves Guillemot', 'T104', 'EVANGELISTA, JEFFYB.', 23.00,  '6-Jan-2020', 'T06'),
+
+-- ('S2939', 'Sundar Pichai', 'T101', 'PRIETO, JOHN CHRISTIAN A', 10.00,  '6-Jan-2020', 'T06'),
+
+-- ('S1521', 'Susan Wojcicki', 'T102', 'INGENTE, JOSHUA B.', 9.00,  '15-Jan-2020', 'T52'),
+
+-- ('S1008', 'Yves Guillemot', 'T104', 'VILLAFLORES, JANMILLENA J.', 23.00, '6-Jan-2020', 'T06'),
+
+-- ('S2939', 'Sundar Pichai', 'T101', 'LALIS, JULLYLLAMARIE M.', 10.00,  '15-Jan-2020', 'T06'),
+
+-- ('S5323', 'Bill Gates', 'T110', 'VISAYA, JESSE JR. B.', 9.00,  '15-Jan-2020' , 'T52'),
+
+
+-- ('S1004', 'Bob Iger', 'T103', 'YEE, JOERETZ O.', 23.00,  '6-Jan-2020', 'T06')
+
+
+select * from public.review
+
